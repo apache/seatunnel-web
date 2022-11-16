7523ff0e0e3f60c8069bcea7f2c07e469e9dfa17
gaojun <gaojun2048@gmail.com>
2022-11-16 18:23:33 +0800

    remove no use file

--- seatunnel-server/seatunnel-app/src/main/java/org/apache/seatunnel/app/thirdparty/framework/OptionRuleWrapper.java
+++
@@ -1,8 +0,0 @@
-package org.apache.seatunnel.app.thirdparty.framework;
-
-/**
- * This class is used to wrapper the seatunnel option rules to seatunnel web form information
- */
-public class OptionRuleWrapper {
-
-}
