a5d4ea6a57e2b892467fed97090bde6f45be3e32
zhujinming <zhujinming@accesscorporate.com.cn>
2023-08-09 18:03:36 +0800

    [Hotfix] Fix admin user for using mysql

--- seatunnel-server/seatunnel-app/src/main/resources/script/seatunnel_server_mysql.sql
+++ seatunnel-server/seatunnel-app/src/main/resources/script/seatunnel_server_mysql.sql
@@ -450,4 +450,10 @@ CREATE TABLE `user_login_log`  (
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 106 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
+-- ----------------------------
+-- Records of user_login_log
+-- ----------------------------
+
+INSERT INTO `seatunnel`.`user`(`username`,`password`,`status`,`type`) values ('admin', '7f97da8846fed829bb8d1fd9f8030f3b', 0, 0);
+
 SET FOREIGN_KEY_CHECKS = 1;
