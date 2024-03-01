d1e5327cc9c6d1e50dc87b842b3ee31b4d14d5e4
hunter-cloud09 <liuhunter09@gmail.com>
2024-03-01 14:25:38 +0800

    reset something

--- seatunnel-server/seatunnel-app/src/main/java/org/apache/seatunnel/app/service/impl/JobExecutorServiceImpl.java
+++ seatunnel-server/seatunnel-app/src/main/java/org/apache/seatunnel/app/service/impl/JobExecutorServiceImpl.java
@@ -76,9 +76,9 @@ public class JobExecutorServiceImpl implements IJobExecutorService {
     }
 
     public String writeJobConfigIntoConfFile(String jobConfig, Long jobDefineId) {
-        String projectRoot = System.getProperty("SEATUNNEL_HOME");
+        String projectRoot = System.getProperty("user.dir");
         String filePath =
-                projectRoot + File.separator + "json" + File.separator + jobDefineId + ".conf";
+                projectRoot + File.separator + "profile" + File.separator + jobDefineId + ".conf";
         try {
             File file = new File(filePath);
             if (!file.exists()) {
