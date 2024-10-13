098d2a279f876b337ceb5efc57fe7d2f2fb56b54
Mohammad Arshad <arshad@apache.org>
2024-10-13 22:07:42 +0530

    [Improvement][Seatunnel-web] Bump seatunnel version from 2.3.7 to 2.3.8

--- seatunnel-server/seatunnel-app/src/main/java/org/apache/seatunnel/app/service/impl/JobDefinitionServiceImpl.java
+++ seatunnel-server/seatunnel-app/src/main/java/org/apache/seatunnel/app/service/impl/JobDefinitionServiceImpl.java
@@ -91,7 +91,7 @@ public class JobDefinitionServiceImpl extends SeatunnelBaseServiceImpl
                 .name(DEFAULT_VERSION)
                 .id(uuid)
                 .engineName(EngineType.SeaTunnel)
-                .engineVersion("2.3.7");
+                .engineVersion("2.3.8");
         if (BusinessMode.DATA_INTEGRATION.equals(jobReq.getJobType())) {
             builder.jobMode(JobMode.BATCH);
         } else if (BusinessMode.DATA_REPLICA.equals(jobReq.getJobType())) {
