5826d483f3b2e2155b8ba26188be8cab2e329bc4
iam11shan <pluson@126.com>
2025-04-07 18:08:13 +0800

    [Refactor] Remove redundant log statements
    Removed unnecessary log messages to improve code readability and maintainability.

--- seatunnel-server/seatunnel-app/src/main/java/org/apache/seatunnel/app/thirdparty/datasource/impl/IcebergDataSourceConfigSwitcher.java
+++ seatunnel-server/seatunnel-app/src/main/java/org/apache/seatunnel/app/thirdparty/datasource/impl/IcebergDataSourceConfigSwitcher.java
@@ -14,15 +14,8 @@ import org.apache.seatunnel.app.thirdparty.datasource.AbstractDataSourceConfigSw
 import org.apache.seatunnel.app.thirdparty.datasource.DataSourceConfigSwitcher;
 import org.apache.seatunnel.common.constants.PluginType;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.google.auto.service.AutoService;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.List;
 
 @AutoService(DataSourceConfigSwitcher.class)
@@ -30,8 +23,6 @@ public class IcebergDataSourceConfigSwitcher extends AbstractDataSourceConfigSwi
     private static final String WAREHOUSE_URI = "warehouse";
     private static final String CATALOG_TYPE = "catalog_type";
 
-    private static final Logger LOGGER =
-            LoggerFactory.getLogger(IcebergDataSourceConfigSwitcher.class);
 
     @Override
     public String getDataSourceName() {
@@ -49,7 +40,7 @@ public class IcebergDataSourceConfigSwitcher extends AbstractDataSourceConfigSwi
             List<RequiredOption> addRequiredOptions,
             List<Option<?>> addOptionalOptions,
             List<String> excludedKeys) {
-        // 根据需要添加或排除特定的配置选项
+
         return super.filterOptionRule(
                 connectorName,
                 dataSourceOptionRule,
@@ -62,16 +53,6 @@ public class IcebergDataSourceConfigSwitcher extends AbstractDataSourceConfigSwi
                 excludedKeys);
     }
 
-    private void logToFile(String message) {
-        try (BufferedWriter writer =
-                new BufferedWriter(new FileWriter(new File("/home/log.log"), true))) {
-            writer.write(message);
-            writer.newLine();
-        } catch (IOException e) {
-            System.err.println("Error writing to log file: " + e.getMessage());
-        }
-    }
-
     @Override
     public Config mergeDatasourceConfig(
             Config dataSourceInstanceConfig,
@@ -81,39 +62,6 @@ public class IcebergDataSourceConfigSwitcher extends AbstractDataSourceConfigSwi
             BusinessMode businessMode,
             PluginType pluginType,
             Config connectorConfig) {
-        logToFile("0dataSourceInstanceConfig:" + dataSourceInstanceConfig);
-        logToFile("0virtualTableDetail:" + virtualTableDetail);
-        logToFile("0dataSourceOption:" + dataSourceOption);
-        logToFile("0selectTableFields:" + selectTableFields);
-        logToFile("0businessMode:" + businessMode);
-        logToFile("0pluginType:" + pluginType);
-        logToFile("0connectorConfig:" + connectorConfig);
-
-        LOGGER.info("0dataSourceInstanceConfig:" + dataSourceInstanceConfig);
-        LOGGER.info("0virtualTableDetail:" + virtualTableDetail);
-        LOGGER.info("0dataSourceOption:" + dataSourceOption);
-        LOGGER.info("0selectTableFields:" + selectTableFields);
-        LOGGER.info("0businessMode:" + businessMode);
-        LOGGER.info("0pluginType:" + pluginType);
-        LOGGER.info("0connectorConfig:" + connectorConfig);
-        // 确保 warehouse 和 catalog_type 配置项存在
-        //        if (!dataSourceInstanceConfig.hasPath(WAREHOUSE_URI)
-        //                || !dataSourceInstanceConfig.hasPath(CATALOG_TYPE)) {
-        //            throw new IllegalArgumentException(
-        //                    "Missing required configuration for 'warehouse' or 'catalog_type'");
-        //        }
-        //
-        //        // 直接从 dataSourceInstanceConfig 获取配置项
-        //        connectorConfig =
-        //                connectorConfig.withValue(
-        //                        WAREHOUSE_URI,
-        //                        ConfigValueFactory.fromAnyRef(
-        //                                dataSourceInstanceConfig.getString(WAREHOUSE_URI)));
-        //        connectorConfig =
-        //                connectorConfig.withValue(
-        //                        CATALOG_TYPE,
-        //                        ConfigValueFactory.fromAnyRef(
-        //                                dataSourceInstanceConfig.getString(CATALOG_TYPE)));
 
         return super.mergeDatasourceConfig(
                 dataSourceInstanceConfig,
