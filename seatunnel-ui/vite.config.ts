a21c27c3b3c6d36e7e09402de8e5aa9976edf9ee
Jast <745925668@qq.com>
2025-07-23 04:40:12 +0800

    [Improve] Add configuration files and update dependencies for enhanced development experience

--- seatunnel-ui/vite.config.ts
+++ seatunnel-ui/vite.config.ts
@@ -18,40 +18,35 @@
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import viteCompression from 'vite-plugin-compression'
 import path from 'path'
 
-export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/ui/' : '/',
-  plugins: [
-    vue(),
-    vueJsx(),
-    viteCompression({
-      verbose: true,
-      disable: false,
-      threshold: 10240,
-      algorithm: 'gzip',
-      ext: '.gz',
-      deleteOriginFile: false
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js',
-      '@intlify/shared': '@intlify/shared/dist/shared.cjs.js'
-    }
-  },
-  server: {
-    proxy: {
-      '/seatunnel/api/v1': {
-        target: loadEnv('development', './').VITE_APP_DEV_WEB_URL,
-        changeOrigin: true
-      },
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    base: process.env.NODE_ENV === 'production' ? '/ui/' : '/',
+    plugins: [
+      vue(),
+      vueJsx()
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js',
+        '@intlify/shared': '@intlify/shared/dist/shared.cjs.js'
+      }
+    },
+    server: {
+      proxy: {
+        '/seatunnel/api/v1': {
+          target: env.VITE_APP_DEV_WEB_URL || 'http://127.0.0.1:8801',
+          changeOrigin: true
+        },
+        '/api': {
+          target: 'http://localhost:8080',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
       }
     }
   }
